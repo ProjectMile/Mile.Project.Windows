8ce214ad1a919e7097cc0ce3d681d3e642a40778
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-09 22:40:11 +0800

    Add InstallTemplate.cmd.

---
+++ InstallTemplate.cmd
@@ -0,0 +1,19 @@
+@rem 
+@rem PROJECT:   Mouri Internal Library Essentials
+@rem FILE:      InstallTemplate.cmd
+@rem PURPOSE:   Install template to project script
+@rem 
+@rem LICENSE:   The MIT License
+@rem 
+@rem DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+@rem 
+
+@echo off
+
+copy "%~dp0.editorconfig" "%~dp0..\"
+copy "%~dp0.gitignore" "%~dp0..\"
+copy "%~dp0ProjectTemplate\BuildAllTargets.cmd" "%~dp0..\"
+copy "%~dp0ProjectTemplate\BuildAllTargets.proj" "%~dp0..\"
+copy "%~dp0ProjectTemplate\Directory.Build.props" "%~dp0..\"
+
+echo Install completed.
